b19032e48660666aca210307113f01df7ad4080c
noahcarrier1999 <car20111@byui.edu>
2023-10-09 12:42:31 -0600

    I finished the learning 03 activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Fraction f1 = new Fraction();
+        System.Console.WriteLine(f1.GetFractionString());
+        System.Console.WriteLine(f1.GetDecimalValue());
+
+        Fraction f2 = new Fraction(6);
+        System.Console.WriteLine(f2.GetFractionString());
+        System.Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(6,7);
+        System.Console.WriteLine(f3.GetFractionString());
+        System.Console.WriteLine(f3.GetDecimalValue());
+
     }
 }
